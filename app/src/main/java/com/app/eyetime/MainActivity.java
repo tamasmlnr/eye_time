9b0dd4782d2924dec8250705066819e304433f0d
Tamás <molnart90@gmail.com>
2019-05-31 16:58:59 +0200

    Add NumberPicker to activity_main

--- app/src/main/java/com/app/eyetime/MainActivity.java
+++ app/src/main/java/com/app/eyetime/MainActivity.java
@@ -2,6 +2,8 @@ package com.app.eyetime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NumberPicker np = findViewById(R.id.numberPicker);
+
+        np.setMinValue(1);
+        np.setMaxValue(30);
+
+        np.setOnValueChangedListener(onValueChangeListener);
     }
+
+    NumberPicker.OnValueChangeListener onValueChangeListener =
+            new NumberPicker.OnValueChangeListener() {
+                @Override
+                public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                    Toast.makeText(MainActivity.this,
+                            "selected number " + numberPicker.getValue(), Toast.LENGTH_SHORT);
+                }
+            };
 }
+
