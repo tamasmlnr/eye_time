51e95d1aa0cdc7e911637449a17458b7833b02e4
Tamás <molnart90@gmail.com>
2019-06-01 08:07:23 +0200

    Add setValues in MainActivity.java

--- app/src/main/java/com/app/eyetime/MainActivity.java
+++ app/src/main/java/com/app/eyetime/MainActivity.java
@@ -2,31 +2,28 @@ package com.app.eyetime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NumberPicker np;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        NumberPicker np = findViewById(R.id.numberPicker);
-
+        np = findViewById(R.id.numberPicker);
         np.setMinValue(1);
         np.setMaxValue(30);
-
-        np.setOnValueChangedListener(onValueChangeListener);
     }
 
-    NumberPicker.OnValueChangeListener onValueChangeListener =
-            new NumberPicker.OnValueChangeListener() {
-                @Override
-                public void onValueChange(NumberPicker numberPicker, int i, int i1) {
-                    Toast.makeText(MainActivity.this,
-                            "selected number " + numberPicker.getValue(), Toast.LENGTH_SHORT);
-                }
-            };
+    public void setValues(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                String.format("Reminder set to %s minutes", np.getValue())
+                , Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
 
