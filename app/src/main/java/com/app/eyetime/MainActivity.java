ec450abc1abf641c5068903d4921471364b89d62
Tamás <molnart90@gmail.com>
2019-06-01 08:33:30 +0200

    Add notifications

--- app/src/main/java/com/app/eyetime/MainActivity.java
+++ app/src/main/java/com/app/eyetime/MainActivity.java
@@ -1,22 +1,33 @@
 package com.app.eyetime;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private NumberPicker np;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
+    private Button button_set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         np = findViewById(R.id.numberPicker);
+        button_set = findViewById(R.id.button_set);
         np.setMinValue(1);
         np.setMaxValue(30);
+        createNotificationChannel();
     }
 
     public void setValues(View view) {
@@ -25,5 +36,36 @@ public class MainActivity extends AppCompatActivity {
                 , Toast.LENGTH_SHORT);
         toast.show();
     }
+
+    public void createNotificationChannel() {
+        mNotifyManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager
+                    .IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    public void sendNotification(View view) {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder() {
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_eye);
+        return notifyBuilder;
+    }
+
 }
 
---
+++ app/src/main/java/com/app/eyetime/NotificationManager.java
@@ -0,0 +1,5 @@
+package com.app.eyetime;
+
+public class NotificationManager {
+
+}
